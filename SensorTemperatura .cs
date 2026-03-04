dde4362db073ebafd254c9f7698fc98f4d452cc6
Hiago 7k <hiago7k@gmail.com>
2026-03-04 20:45:22 -0300

    criando nossa clase sensortemperatura e implementando interface iSensor

---
+++ SensorTemperatura .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticnado_HEranca_com_Interface
+{
+    internal class SensorTemperatura
+    {
+    }
+}
