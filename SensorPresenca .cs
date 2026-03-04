556fd7f805383b7c31bf6325c89f44fcda0519af
Hiago 7k <hiago7k@gmail.com>
2026-03-04 20:49:00 -0300

    implemtnado metodo ativar e desativar na classe

--- SensorPresenca .cs
+++ SensorPresenca .cs
@@ -3,4 +3,19 @@ namespace Praticnado_HEranca_com_Interface;
 
 internal class SensorPresenca : ISensor
 {
+
+    public bool Ativar()
+    {
+        Console.WriteLine("Sensor de presenca Ativado");
+        return true;
+        
+    }
+
+    public bool Desativar()
+    {
+        Console.WriteLine("Sensor de presenca desativado");
+        return false;
+
+    }
+
 }
